8052ca9c91000bb84f150f0dfeffc12a7939428a
George Galvin <george.galvin1996@gmail.com>
2019-07-10 14:59:47 +0100

    Add files via upload

--- apophis_propagation_v3.py
+++ apophis_propagation_v3.py
@@ -4,13 +4,13 @@ import matplotlib.pyplot as plt
 import time as tm
 import spiceypy as spice
 
-from astropy.constants import M_sun, M_earth, GM_sun, au
-from scipy.integrate import ode, solve_ivp
+from scipy.integrate import solve_ivp
 from MultistepRadau import MultistepRadau
 from numpy.linalg import norm
+from astroquery.jplhorizons import Horizons
 
 spice.furnsh("de405.bsp")
-spice.furnsh("naif0009.tls")
+spice.furnsh("naif0011.tls")
 
 au = 149597870691
 c = 299792458
@@ -18,10 +18,11 @@ c = 299792458
 '''STATE VECTORS'''
 
 #Initial conditions from Giorgini (2008), September 1 2006 00:00
-#state_vector_2006 = [77727856485.2246, 97506471809.9321, 38271666051.90773, \
-#-22433.451271099162, 22780.815403058397, 7899.677821557445]
+#state_vector_2006 = [77727856480.57489, 97506479556.85107, 38271646296.414696,\
+#-22433.451273293453, 22780.817013138632, 7899.673185220072]
 
-state_vector_2006 = [77727856480.57504, 97506471804.09593, 38271666049.61714, -22433.45127329345, 22780.815405286692, 7899.677822330075]
+state_vector_2006 = [77727856999.78587, 97506479057.6083, 38271646074.326355, \
+                     -22433.451264384308, 22780.817020556697, 7899.673188033485]
 
 #HORIZONS state vector, September 1 2006 00:00
 state_vector_2006_horizons = [77727900266.26141, 97506424400.57415, 38271670918.17467, \
@@ -31,10 +32,14 @@ state_vector_2006_horizons = [77727900266.26141, 97506424400.57415, 38271670918.
 state_vector_2019_horizons = [110902901314.6609, 38080320887.97272, 17020446307.60740, \
  -7880.349990261348, 32832.95238798142, 12006.39719779215]
 
+state_vector_2029_horizons = [-81358242708.21650, 128849750172.9417, 45816260019.76273, \
+-24168.24085388422, -10019.52130959752, -4330.751598587176]
+
+
 ''' Times - Seconds past JD 0 '''
-#t_2006 = 2453979.5 * 86400 #September 1.0, 2006 UTC
-t_2006 = spice.str2et('Sep 1, 2006')
-t_test = t_2006 + ((5)*86400) #Test time, can be changed
+t_2006 = (2453979.5-2451545.0) * 86400 #September 1.0, 2006 UTC
+#t_2006 = spice.str2et('Sep 1, 2006')
+t_test = spice.str2et('Jan 1, 2029') #Test time, can be changed
 t_2029_before = spice.str2et('Apr 8, 2029')
 t_2029_after = spice.str2et('April 18, 2029')
 
@@ -43,41 +48,29 @@ seconds_per_day = 86400
 
 class ApophisPropagation():
 
-    '''gm_dictionary = {
+    gm_dictionary = {
         "sun": 1.32712440017987e20,
-        "mercury barycenter": 2.2032080e13,
-        "venus barycenter": 3.24858599e14,
-        "earth barycenter": 3.98600433e14,
+        "mercury": 2.2032080e13,
+        "venus": 3.24858599e14,
+        "earth": 3.98600433e14,
         "moon":  4.902801e12,
-        "mars barycenter": 4.2828314e13,
+        "mars": 4.2828314e13,
         "jupiter barycenter": 1.26712767863e17,
         "saturn barycenter": 3.7940626063e16,
         "uranus barycenter": 5.794549007e15,
         "neptune barycenter": 6.836534064e15,
         "pluto barycenter": 9.81601e11
-    }'''
-
-    gm_dictionary = {
-        "sun": 0.01720209895**2 * (au**3) / (86400**2),
-        "mercury barycenter": 0.4912547451450812e-10 * (au**3) / (86400**2),
-        "venus barycenter": 0.7243452486162703e-9 * (au**3) / (86400**2),
-        "earth barycenter": 0.8887692390113509e-9 * (au**3) / (86400**2),
-        "moon": 0.1093189565989898e-10 * (au**3) / (86400**2),
-        "mars barycenter": 0.9549535105779258e-10 * (au**3) / (86400**2),
-        "jupiter barycenter": 0.2825345909524226e-6 * (au**3) / (86400**2),
-        "saturn barycenter": 0.8459715185680659e-7 * (au**3) / (86400**2),
-        "uranus barycenter": 0.1292024916781969e-7 * (au**3) / (86400**2),
-        "neptune barycenter": 0.1524358900784276e-7 * (au**3) / (86400**2),
-        "pluto barycenter": 0.2188699765425970e-11 * (au**3) / (86400**2)
     }
-
     def __init__(self, initial_state_vector, start_time):
         self.initial_state_vector = initial_state_vector
         self.start_time = start_time
-        print(self.gm_dictionary)
 
     def name(self):
         return "Apophis"
+
+    def ecl_to_eq(self, vec, ob=m.radians(23.4392911111)):
+        result = np.array([vec[0], m.cos(ob)*vec[1] - m.sin(ob)*vec[2], m.sin(ob)*vec[1] + m.cos(ob)*vec[2]])
+        return result
 
     def _gravity_newtonian(self, r, planet, t):
         '''Newtonian gravitational acceleration from specified planet.
@@ -108,25 +101,27 @@ class ApophisPropagation():
 
         return np.append(rdot, vdot)
 
-    def _get_body_barycentric_acc(self, body, time):
+    def _get_body_barycentric_acc(self, body, t):
         #Preliminary: uses numerical differentiation to estimate body acceleration
-        interval = TimeDelta(1, format="jd")
-        v1 = get_body_barycentric_posvel(body, time-interval)[1]
-        v2 = get_body_barycentric_posvel(body, time+interval)[1]
+        interval = 86400
+        v1 = spice.spkezr(body, t-interval, 'J2000', 'NONE', 'solar system barycenter')[0][3:6] * 1000
+        v2 = spice.spkezr(body, t+interval, 'J2000', 'NONE', 'solar system barycenter')[0][3:6] * 1000
         return (v2-v1)/(2*interval)
 
-    def _quantity_sum(self, q): #As the numpy.sum function doesn't work with Quantity objects
-        q_sum = 0
-        for i in q:
-            q_sum += i
-        return q_sum
-
     def _distance_from_earth(self, r, t):
-        #time_object = Time(t/86400, format="jd")
-        #earth_position = get_body_barycentric("earth", time_object)
-        #earth_position_nd = earth_position.get_xyz().to(u.m).value
         earth_position = spice.spkpos("earth", t, "J2000", 'NONE', 'SOLAR SYSTEM BARYCENTER')[0] * 1000
         return np.linalg.norm(r - earth_position)
+
+    def _distance_from_horizons(self, r, t):
+        t_jd = float(spice.et2utc(t, "j", 7)[3:18])
+        a_eph = Horizons(id='Apophis', location = '@0', epochs=t_jd)
+        v_eph = a_eph.vectors()
+        v_eph['x'].convert_unit_to('m')
+        v_eph['y'].convert_unit_to('m')
+        v_eph['z'].convert_unit_to('m')
+        eq = self.ecl_to_eq(np.array([float(v_eph['x']), float(v_eph['y']), float(v_eph['z'])]))
+
+        return norm(r - eq)
 
     def _total_gravity_relativistic(self, t, y):
         '''Calculates the gravitational acceleration on a body with given
@@ -137,58 +132,57 @@ class ApophisPropagation():
         beta = 1
         gamma = 1
         g_sum = [0, 0, 0]
-        m=[]
+        gm=[]
         r_vec=[]
         r=[]
         v_vec=[]
         v=[]
         a_vec=[]
-        time = Time(t/86400, format="jd")
 
-        for planet in self.mass_dictionary:
-            m.append(self.mass_dictionary[planet])
-            posvel = get_body_barycentric_posvel(planet, time)
-            r_planet = posvel[0].get_xyz().to(u.m).value
-            v_planet = posvel[1].get_xyz().to(u.m/u.s).value
+        for planet in self.gm_dictionary:
+            gm.append(self.gm_dictionary[planet])
+            posvel = spice.spkezr(planet, t, 'J2000', 'NONE', 'solar system barycenter')[0] * 1000
+            r_planet = posvel[0:3]
+            v_planet = posvel[3:6]
 
             r_vec.append(r_planet)
             v_vec.append(v_planet)
             r.append(norm(r_planet - r_apophis))
             v.append(norm(v_planet))
-            a_vec.append(self._get_body_barycentric_acc(planet, time).get_xyz().to(u.m/u.s**2).value)
+            a_vec.append(self._get_body_barycentric_acc(planet, t))
 
-        term2 = (-2 * G.value * (beta + gamma) / c.value**2) * self._quantity_sum([c/d for c, d in zip(m, r)])
-        term4 = gamma * norm(v_apophis)**2 / c.value**2
+        term2 = (-2 * (beta + gamma) / c**2) * np.sum([x/y for x, y in zip(gm, r)])
+        term4 = gamma * norm(v_apophis)**2 / c**2
 
         i = 0
-        for body in m:
-            term1 = m[i]*(r_vec[i] - r_apophis) / r[i]**3
+        for body in gm:
+            term1 = gm[i]*(r_vec[i] - r_apophis) / r[i]**3
 
             term3_prelim = 0
 
             j = 0
-            for body2 in m:
+            for body2 in gm:
                 if body != body2:
-                    term3_prelim += m[j] / norm(r_vec[i] - r_vec[j])
+                    term3_prelim += gm[j] / norm(r_vec[i] - r_vec[j])
                 j += 1
 
-            term3 = term3_prelim * (1-2*beta) * G.value / c.value**2
+            term3 = term3_prelim * (1-2*beta) / c**2
 
-            term5 = (1+gamma) * v[i]**2 / c.value**2
+            term5 = (1+gamma) * v[i]**2 / c**2
 
-            term6 = (np.dot(v_apophis, v_vec[i]) * (-2 * (1+gamma)) / c.value**2)
+            term6 = (np.dot(v_apophis, v_vec[i]) * (-2 * (1+gamma)) / c**2)
 
-            term7 = (np.dot(r_apophis - r_vec[i], v_vec[i])/r[i])**2 * (-1.5 / c.value**2)
+            term7 = (np.dot(r_apophis - r_vec[i], v_vec[i])/r[i])**2 * (-1.5 / c**2)
 
-            term8 = (np.dot(r_vec[i]-r_apophis, a_vec[i]) / (2 * c.value**2))
+            term8 = (np.dot(r_vec[i]-r_apophis, a_vec[i]) / (2 * c**2))
 
-            term9 = m[i] / (c.value**2 * r[i])
+            term9 = gm[i] / (c**2 * r[i])
 
             term10 = a_vec[i] * (3+4*gamma) / 2
 
             term11 = np.dot((np.multiply(2+2*gamma, v_apophis) - (1+2*gamma)*v_vec[i]), r_apophis-r_vec[i]) * (v_apophis - v_vec[i]) / r[i]**2
 
-            g_sum = g_sum + G.value*term1*(1 + term2 + term3 + term4 + term5 + term6 + term7 + term8) + G.value*term9*(term10 + term11)
+            g_sum = g_sum + term1*(1 + term2 + term3 + term4 + term5 + term6 + term7 + term8) + term9*(term10 + term11)
 
             i += 1
 
@@ -206,9 +200,9 @@ class ApophisPropagation():
 
         term1 = self._total_gravity_newtonian(t, y)[3:6]
 
-        term2 = GM_sun.value/(c**2 * norm(r_apophis_sun)**3)
+        term2 = self.gm_dictionary["sun"]/(c**2 * norm(r_apophis_sun)**3)
 
-        term3 = ((4 * GM_sun.value / norm(r_apophis_sun)) - norm(v_apophis_sun)**2)*r_apophis_sun
+        term3 = ((4 * self.gm_dictionary["sun"] / norm(r_apophis_sun)) - norm(v_apophis_sun)**2)*r_apophis_sun
 
         term4 = 4*np.dot(r_apophis_sun, v_apophis_sun)*v_apophis_sun
 
@@ -220,26 +214,7 @@ class ApophisPropagation():
     def all_gravities(self):
         print(np.ndarray.tolist(self._total_gravity_newtonian(self.start_time, self.initial_state_vector)))
         print(np.ndarray.tolist(self._total_gravity_relativistic_light(self.start_time, self.initial_state_vector)))
-        print(np.ndarray.tolist(self._total_gravity_relativistic_med(self.start_time, self.initial_state_vector)))
         print(np.ndarray.tolist(self._total_gravity_relativistic(self.start_time, self.initial_state_vector)))
-
-    def Propagate(self, finish_time, step_time, gravity_model="relativistic_light"):
-        if gravity_model == "newtonian":
-            rhs = self._total_gravity_newtonian
-        elif gravity_model == "relativistic":
-            rhs = self._total_gravity_relativistic
-        elif gravity_model == "relativistic_light":
-            rhs = self._total_gravity_relativistic_light
-        t0 = tm.clock()
-        times = []
-        distances_from_earth = []
-        test = ode(rhs)
-        test.set_integrator("dopri5")
-        test.set_initial_value(self.initial_state_vector, self.start_time)
-
-        while test.successful() and test.t < finish_time:
-            test.integrate(test.t+step_time)
-        return(test.y)
 
     def PropagateMCM(self, finish_time, step_time, gravity_model="relativistic_light"):
         if gravity_model == "newtonian":
@@ -252,40 +227,6 @@ class ApophisPropagation():
         t, y = mcm.Integrate()
 
         return y[:,-1]
-
-    def ClosestApproach(self, step_time, gravity_model="relativistic_light"):
-        if gravity_model == "newtonian":
-            rhs = self._total_gravity_newtonian
-        elif gravity_model == "relativistic":
-            rhs = self._total_gravity_relativistic
-        elif gravity_model == "relativistic_light":
-            rhs = self._total_gravity_relativistic_light
-
-        finish_time = t_2029_after
-
-        t0 = tm.perf_counter()
-        times = []
-        distances_from_earth = []
-        test = ode(rhs)
-        test.set_integrator("dopri5")
-        test.set_initial_value(self.initial_state_vector, self.start_time)
-        while test.successful() and test.t < t_2029_after:
-            if (test.t < t_2029_before):
-                test.integrate(test.t+step_time)
-            elif (test.t > 2462239.5*86400) and (test.t < 2462240.5*86400):
-                test.integrate(test.t+step_time/800)
-                distances_from_earth.append(self._distance_from_earth(test.y[0:3], test.t))
-                times.append(test.t / 86400)
-            else:
-                test.integrate(test.t+step_time/100)
-                distances_from_earth.append(self._distance_from_earth(test.y[0:3], test.t))
-                times.append(test.t / 86400)
-
-        x = np.argmin(distances_from_earth)
-        print("Closest approach: ", distances_from_earth[x], "metres at JD", times[x])
-        print("Propagation time:", tm.perf_counter() - t0)
-        plt.plot(times, distances_from_earth)
-        plt.show()
 
     def ClosestApproachScipy(self, step_time, gravity_model="relativistic_light", integrator="RK45"):
         if gravity_model == "newtonian":
@@ -301,6 +242,16 @@ class ApophisPropagation():
         t1 = ivp1.t
         y1 = ivp1.y
 
+        #distances_from_horizons = []
+        #times = []
+
+        #for i in range(len(y1[0])):
+        #    distances_from_horizons.append(self._distance_from_horizons(y1[0:3, i], t1[i]))
+        #    times.append(t1[i])
+        #plt.plot(times, distances_from_horizons)
+        #plt.show()
+
+        ####################################
         ivp2 = solve_ivp(rhs, (t1[-1], t_2029_after), y0=y1[:, -1], method=integrator, max_step=step_time/100)
         t2 = ivp2.t
         y2 = ivp2.y
@@ -310,7 +261,7 @@ class ApophisPropagation():
 
         for i in range(len(y2[0])):
             distances_from_earth.append(self._distance_from_earth(y2[0:3, i], t2[i]))
-            times.append(t2[i]/86400)
+            times.append(t2[i])
 
         x = np.argmin(distances_from_earth)
 
@@ -327,7 +278,7 @@ class ApophisPropagation():
 
         for i in range(len(y3[0])):
             distances_from_earth.append(self._distance_from_earth(y3[0:3, i], t3[i]))
-            times.append(t3[i]/86400)
+            times.append(t3[i])
 
         x = np.argmin(distances_from_earth)
 
@@ -344,12 +295,11 @@ class ApophisPropagation():
 
         for i in range(len(y4[0])):
             distances_from_earth.append(self._distance_from_earth(y4[0:3, i], t4[i]))
-            times.append(t4[i]/86400)
+            times.append(t4[i])
 
         x = np.argmin(distances_from_earth)
 
-
-        print("Closest approach: ", distances_from_earth[x], "metres at JD", times[x])
+        print("Closest approach: ", distances_from_earth[x], "metres at", (times[x]/86400)+2451545)#spice.et2utc(times[x], 'J', 10))
         print("Propagation time:", tm.perf_counter() - start_clock)
 
         plt.plot(times, distances_from_earth)
@@ -420,7 +370,7 @@ class ApophisPropagation():
         #earth_v_nd = earth_v.get_xyz().to(u.m/u.s).value
         #relative_velocity = np.linalg.norm(v_x - earth_v_nd)
 
-        print("Closest approach: ", distances_from_earth[x], "metres at JD", times[x])
+        print("Closest approach: ", distances_from_earth[x], "metres at ", (times[x]/86400)+2451545)#spice.et2tc(times[x], 'J', 10))
         print("Propagation time:", tm.perf_counter( ) - start_clock)
         #print("Relative velocity:", relative_velocity)
         #print("Maximum discretisation error", relative_velocity * (step_time/2000000))
@@ -429,4 +379,4 @@ class ApophisPropagation():
         return t1, y1
 
 a = ApophisPropagation(state_vector_2006, t_2006)
-a.ClosestApproachScipy(seconds_per_day)
+a.ClosestApproachScipy(seconds_per_day, gravity_model = "relativistic_light")
--- coordinate_conversion_v2.py
+++ coordinate_conversion_v2.py
@@ -1,14 +1,12 @@
 import math as m
-import spiceypy as s
+import spiceypy as spice
 import numpy as np
 import astropy.units as u
 
-from astropy.time import Time
-from astropy.coordinates import SkyCoord, CartesianRepresentation as cr, \
-    get_body_barycentric_posvel, solar_system_ephemeris
-
 au = 149597870691
 GM_sun_km = 1.32712440017987e11
+spice.furnsh('de405.bsp')
+spice.furnsh('naif0009.tls')
 
 def ecl_to_eq(vec, ob=m.radians(23.4392911111111)):
     result = ([vec[0],
@@ -26,13 +24,6 @@ def eq_to_ecl(vec, ob=m.radians(23.4392911111111)):
 '''The raw initial conditions given in the paper. Heliocentric osculating
     Keplerian elements in the J2000 ecliptic frame.'''
 
-#r_p = 0.7460599868324846 #TEST INPUTS
-#e = 0.1910572689856682
-#i = 3.331330601139759
-#asc_node = 204.4602199800390
-#a_of_p = 126.3961940750760
-#mean_anomaly = 61.41675357959579
-
 r_p = 0.7460599319224038 #Perigee radius (AU)
 e = 0.1910573105795565 #Eccentricity
 i = 3.33132242244163 #Inclination (degrees)
@@ -47,20 +38,20 @@ i_r = m.radians(i)
 asc_node_r = m.radians(asc_node)
 a_of_p_r = m.radians(a_of_p)
 mean_anomaly_r = m.radians(mean_anomaly)
-epoch_JD2000_s = (epoch - 2451545.0)*86400
+epoch_JD2000_s = (epoch - 2451545) * 86400 #spice.str2et('jd 2453979.5')
 
 keplerian_elements = [r_p_km, e, i_r, asc_node_r, a_of_p_r, mean_anomaly_r, epoch_JD2000_s, GM_sun_km]
 
 '''First, convert Keplerian elemets to a Cartesian state vector. Spiceypy gives it
     in km and km/s.'''
-sv_helio_ecliptic_km =  s.spiceypy.conics(keplerian_elements, epoch_JD2000_s)
+sv_helio_ecliptic_km =  spice.conics(keplerian_elements, epoch_JD2000_s)
 sv_helio_ecliptic = sv_helio_ecliptic_km * 1000
 
 print("Heliocentric ecliptic cartesian:", np.ndarray.tolist(sv_helio_ecliptic))
 
 '''Then, convert from ecliptic to equatorial frame'''
 
-eps = m.radians(23.4392911111111) #obliquity to the ecliptic
+eps = m.radians(23.43927944444444) #obliquity to the ecliptic
 sv_helio_equatorial = []
 sv_helio_equatorial[0:3] = ecl_to_eq(sv_helio_ecliptic[0:3], eps)
 sv_helio_equatorial[3:6] = ecl_to_eq(sv_helio_ecliptic[3:6], eps)
@@ -68,13 +59,8 @@ sv_helio_equatorial[3:6] = ecl_to_eq(sv_helio_ecliptic[3:6], eps)
 print("Heliocentric equatorial:", sv_helio_equatorial)
 
 '''Then, convert from heliocentric to barycentric coordinates'''
-solar_system_ephemeris.set("URL:https://naif.jpl.nasa.gov/pub/naif/generic_kernels/spk/planets/a_old_versions/de405.bsp")
 
-epoch_time = Time(epoch, format='jd')
-sun_posvel_au_d = get_body_barycentric_posvel('sun', epoch_time) #in AU and AU/d
-sun_posvel = []
-sun_posvel[0:3] = sun_posvel_au_d[0].get_xyz().to(u.m).value
-sun_posvel[3:6] = sun_posvel_au_d[1].get_xyz().to(u.m / u.s).value
+sun_posvel = spice.spkssb(10, epoch_JD2000_s, 'J2000') * 1000
 
 sv = np.add(sv_helio_equatorial, sun_posvel)
 
